8c5b7026b09ceb9096e187cadc06e7f52e919c25
609923924 <609923924@qq.com>
2022-06-23 19:24:53 +0800

    Site updated: 2022-06-23 19:24:53

--- js/third-party/comments/isso.js
+++ js/third-party/comments/isso.js
@@ -4,7 +4,8 @@ document.addEventListener('page:loaded', () => {
   if (!CONFIG.page.comments) return;
 
   NexT.utils.loadComments('#isso-thread')
-    .then(() => NexT.utils.getScript(`${CONFIG.isso}js/embed.min.js`, {
+    .then(() => NexT.utils.getScript(`${CONFIG.isso}/js/embed.min.js`, 
+    {
       attributes: {
         dataset: {
           isso: `${CONFIG.isso}`
